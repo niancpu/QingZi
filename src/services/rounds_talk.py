fbf5551620bc544b17116527bad0e1e6d56ec975
niancpu <1756583517@qq.com>
2026-04-27 22:56:33 +0800

    用来实现读取写入历史对话为json，算是资源库

---
+++ src/services/rounds_talk.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import threading
+from typing import TYPE_CHECKING
+from datetime import datetime
+
+from dao import json_oper
+from config import paths
+
+_lock=threading.Lock()
+#定义一个排他锁
+
+class TalkHistory:
+    def __init__(self):
+        _time_name=datetime.now().strftime("%Y%m%d_%H%M%S")
+        _path:Path=paths.get_home_path()/_time_name
+
+    if TYPE_CHECKING:
+        from server.routes.base import ChatInput
+
+    def save_talk(self,chat_input:'ChatInput',reply:str)->None:
+        delta=json_oper.json_loader(TalkHistory._path)
+        with _lock: 
+            json_oper.json_check_writer(TalkHistory._path,delta)
+    
+    def read_talk(self)->dict:
+        data:dict=json_oper.json_loader(TalkHistory._path)
+        return data
+history=TalkHistory()
+
+
